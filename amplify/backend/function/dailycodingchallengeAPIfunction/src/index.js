6641da28eaeb3e9a4175793ca5062e7a3be8812c
Mihailo <mzecar92@gmail.com>
2020-12-01 09:44:02 +0100

    added api

---
+++ amplify/backend/function/dailycodingchallengeAPIfunction/src/index.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  
+  return response;
+};
