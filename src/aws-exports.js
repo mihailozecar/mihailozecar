dd9a47a145ccac683b8411d1ed47361b5aa34df5
Mihailo <mzecar92@gmail.com>
2021-05-13 15:44:21 +0200

    new amplify

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,15 +3,15 @@
 
 const awsmobile = {
     "aws_project_region": "eu-west-1",
-    "aws_cognito_identity_pool_id": "eu-west-1:0be936a6-e767-4b82-875f-34f141e25a49",
+    "aws_cognito_identity_pool_id": "eu-west-1:fef7404d-6da9-49ff-b63a-2851939d7683",
     "aws_cognito_region": "eu-west-1",
-    "aws_user_pools_id": "eu-west-1_yrk1SXoid",
-    "aws_user_pools_web_client_id": "h9h7qiintbg3abuorl6v9233b",
+    "aws_user_pools_id": "eu-west-1_J1bGCbimz",
+    "aws_user_pools_web_client_id": "34qcf7gq82onedvg6cpqr2l1b7",
     "oauth": {},
     "aws_cloud_logic_custom": [
         {
             "name": "dailycodingchallengeWebAPI",
-            "endpoint": "https://mfhfbztu7j.execute-api.eu-west-1.amazonaws.com/prod",
+            "endpoint": "https://02hsjkbw98.execute-api.eu-west-1.amazonaws.com/prod",
             "region": "eu-west-1"
         }
     ]
